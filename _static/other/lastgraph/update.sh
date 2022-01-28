b84571e102190aefa4f2b0ae110fed59efa31a04
S. Zeid <s@zeid.me>
2022-01-27 20:00:50 -0600

    [other/lastgraph] use variable for username

--- _static/other/lastgraph/update.sh
+++ _static/other/lastgraph/update.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-cd "$(dirname "$0")"
-/home/scottywz/bin/private/lastgraph -p '1 year' -s sunset -o graph.svg ScottyWZ >/dev/null
-cp -a graph.svg ../../../_site/other/lastgraph/
+LAST_FM_USERNAME=ScottyWZ
+
+cd "$(dirname -- "$0")"
+
+if [ x"$LAST_FM_USERNAME" != x"" ]; then
+ ~/bin/private/lastgraph -p '1 year' -s sunset -o graph.svg "$LAST_FM_USERNAME" >/dev/null
+ cp -a graph.svg ../../../_site/other/lastgraph/
+else
+ echo "$(basename -- "$0"): error: LAST_FM_USERNAME is empty in this script" >&2
+ exit 2
+fi
